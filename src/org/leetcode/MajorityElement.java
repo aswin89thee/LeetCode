bff4492658e3b2ddaf45a999cb5fbd5d3e3f310a
Aswin Rajamannar <aswin89thee@gmail.com>
2015-05-03 14:28:47 -0400

    Updated Majority Element problem with Moore's voting algorithm

--- src/org/leetcode/MajorityElement.java
+++ src/org/leetcode/MajorityElement.java
@@ -6,7 +6,32 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class MajorityElement {
+	//Using Moore's voting method. O(n) runtime with O(1) space
 	public int majorityElement(int[] nums) {
+        if(nums.length < 1) return 0;
+        if(nums.length == 1) return nums[0];
+        int majElt = -1, majCnt = 0;;
+        majElt = nums[0];
+        majCnt = 1;
+        int i = 0;
+        for(i = 1 ; i < nums.length ; i++){
+        	if(nums[i] == majElt){
+        		majCnt++;
+        		continue;
+        	}else{
+        		majCnt--;
+        		if(majCnt == 0){
+        			majElt = nums[i];
+        			majCnt++;
+        		}
+        	}
+        }
+        //Since the problem statement says that a majority element always exists, we don't have to check if majElt occurs at least (n/2) times
+        return majElt;
+    }
+
+	//Naive algorithm
+	public int majorityElement2(int[] nums) {
 		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
 		for(int cur : nums){
 			if(map.containsKey(cur)){
@@ -32,7 +57,7 @@ public class MajorityElement {
 
 	public static void main(String[] args){
 		MajorityElement obj = new MajorityElement();
-		int[] a = {2,3,4,4,5,4,6,2,2,2};
+		int[] a =  {3,3,4,2,4,4,2,4,4};
 		System.out.println("Majority element is " + obj.majorityElement(a));
 	}
 }
