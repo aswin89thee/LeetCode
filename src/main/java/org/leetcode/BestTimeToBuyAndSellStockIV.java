817f16f3790bd7e5d9d1ac86255f8aca16c3c5a2
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-02 00:55:13 -0400

    Changed "Best Time To Buy And Sell Stock IV"

--- src/main/java/org/leetcode/BestTimeToBuyAndSellStockIV.java
+++ src/main/java/org/leetcode/BestTimeToBuyAndSellStockIV.java
@@ -1,60 +1,74 @@
 package org.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
 
-//Wrong Answer. 200/211 Test cases passed. Not DP solution. I tried to use greedy approach and it failed
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time 
+(ie, you must sell the stock before you buy again).
+ */
 public class BestTimeToBuyAndSellStockIV {
 	
-	public int maxProfit(int k, int[] prices) {
-        int maxProf = 0;
-        if(prices==null || prices.length==0 || k<1) return maxProf;
-        List<Integer> profitList = new ArrayList<Integer>();
-        int begin = 0, end=0;
-        for(int i = 0 ; i < prices.length ; i++){
-        	if(prices[i] < prices[begin]){
-        		begin = end = i;
-        	}
-        	if(prices[i] > prices[end]){
-        		end = i;
-        	}
-        	if(i == prices.length-1){
-        		if(end > begin && prices[end] > prices[begin]){
-        			addToProfitList(prices,begin,end,k,profitList);
-        		}
-        	}else{
-        		if(prices[i+1] < prices[i]){
-        			addToProfitList(prices,begin,end,k,profitList);
-        			begin = end = i+1;
-        		}
-        	}
-        }
-        for(Integer cur : profitList){
-        	maxProf += cur;
-        }
-        return maxProf;
-    }
-
-	private void addToProfitList(int[] prices, int begin, int end, int k, List<Integer> profitList) {
-		int curProfit = prices[end] - prices[begin];
-		if(profitList.size() < k){
-			profitList.add(curProfit);
-			return;
-		}
-		int minVal = Integer.MAX_VALUE;
-		for(Integer cur : profitList){
-			if(cur < minVal){
-				minVal = cur;
+	//Dynamic programming solution. But still time limit exceeded. Works on my test cases
+	public int maxProfit(int k, int[] prices)
+	{
+		if(k <= 0)
+			return 0;
+		if(prices.length <= 1)
+			return 0;
+		Integer[][] profits = new Integer[prices.length][k+1];
+		int maxprofit = getMaxProfit(prices, k, 0, profits);
+		for(int i = 0 ; i < prices.length ; i++)
+		{
+			System.out.println();
+			for(int j = 0 ; j < k+1 ; j++)
+			{
+				System.out.print(j + "\t");
 			}
 		}
-		if(curProfit > minVal){
-			profitList.remove(new Integer(minVal));
-			profitList.add(curProfit);
+		return maxprofit;
+    }
+
+	private int getMaxProfit(int[] prices, int k, int day, Integer[][] profits)
+	{
+		if(k <= 0)
+			return 0;
+		if(day >= prices.length - 1)
+			return 0;
+		if(profits[day][k] != null)
+			return profits[day][k];
+		int maxprofit = Integer.MIN_VALUE;
+		int curprofit = Integer.MIN_VALUE;
+		int min = prices[day];
+		for(int i = day + 1 ; i < prices.length ; i++)
+		{
+			if(prices[i] < min)
+			{
+				min = prices[i];
+				curprofit = 0;
+				continue;
+			}
+			if(prices[i] - min > curprofit)
+			{
+				curprofit = prices[i] - min;
+				int restProfit = getMaxProfit(prices, k-1, i+1, profits);
+				if(curprofit + restProfit > maxprofit)
+					maxprofit = curprofit + restProfit;
+			}
 		}
+		if(maxprofit == Integer.MIN_VALUE)
+			maxprofit = 0;
+		profits[day][k] = maxprofit;
+		return maxprofit;
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)
+	{
+		BestTimeToBuyAndSellStockIV obj = new BestTimeToBuyAndSellStockIV();
+		int[] prices = {5,2,6,3,4,2,1};
+		System.out.println(obj.maxProfit(3, prices));
 	}
 
 }
