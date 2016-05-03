f2455c191aaced664e70934b9aac62a5a4770673
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-03 14:56:01 -0400

    Changed "ReverseBits" to an acceptable solution

--- src/main/java/org/leetcode/ReverseBits.java
+++ src/main/java/org/leetcode/ReverseBits.java
@@ -1,17 +1,28 @@
 package org.leetcode;
 
 public class ReverseBits {
-	public int reverseBits(int n) {
-        long reverseNum = 0;
-        for(int i = 0 ; i < 32 ; i++){
-        	if((n & (1 << i)) > 0){
-        		reverseNum |= (1 << (31-i));
-        	}
-        }
-        return (int)reverseNum;
-    }
 	
-		public static void main(String[] args) {
+	//3ms runtime. Thanks to www.programcreek.com for the solution
+	public int reverseBits(int n) {
+		for (int i = 0; i < 16; i++) {
+			n = swapBits(n, i, 32 - i - 1);
+		}
+
+		return n;
+	}
+
+	public int swapBits(int n, int i, int j) {
+		int a = (n >> i) & 1;
+		int b = (n >> j) & 1;
+
+		if ((a ^ b) != 0) {
+			return n ^= (1 << i) | (1 << j);
+		}
+
+		return n;
+	}
+
+	public static void main(String[] args) {
 		ReverseBits obj = new ReverseBits();
 		System.out.println(obj.reverseBits(43261596));;
 	}
