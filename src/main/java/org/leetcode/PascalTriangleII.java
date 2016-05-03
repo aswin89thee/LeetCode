1aae7db0c2bbde08f0470227c4b393e2dfffe01d
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-02 20:35:14 -0400

    Modified PascalTriangleII to an accepted solution

--- src/main/java/org/leetcode/PascalTriangleII.java
+++ src/main/java/org/leetcode/PascalTriangleII.java
@@ -1,30 +1,42 @@
 package org.leetcode;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+/*
+ * Given an index k, return the kth row of the Pascal's triangle.
 
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+ */
 public class PascalTriangleII {
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public List<Integer> getRow(int rowIndex) {
-        int[] a = new int[rowIndex+1];
-        a[0] = 1;
-        int i,j;
-        if(rowIndex > 1){
-        	for(i=0; i< rowIndex; i++){
-        		for(j=i+1; j>0 ;j--){
-        			a[j] += a[j-1];
-        		}
-        	}
-        }
-        
-        return new ArrayList(Arrays.asList(a));
+	
+	//Accepted solution. 11 ms runtime 
+	//Note: This is a slow solution, since BigDecimal operations are slow
+	public List<Integer> getRow(int rowIndex)
+	{
+		List<Integer> row = new ArrayList<Integer>();
+		row.add(1);
+		for(int i = 1 ; i < rowIndex ; i++)
+		{
+			BigDecimal bd1 = new BigDecimal(row.get(i - 1));
+			BigDecimal bd2 = new BigDecimal(rowIndex - i + 1);
+			BigDecimal bd3 = new BigDecimal(i);
+			BigDecimal bdelt = bd1.multiply(bd2).divide(bd3);
+			row.add(bdelt.intValue());
+		}
+		if(row.size() == rowIndex)
+			row.add(1);
+		return row;
     }
 
 	public static void main(String[] args) {
 		PascalTriangleII obj = new PascalTriangleII();
-		obj.getRow(0);
+		System.out.println(obj.getRow(30));
 	}
 
 }
