48bae164abb23b10cb89b22c95d5301d3010eae2
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-07 16:18:54 -0400

    Fixed "Word Ladder" to an accepted solution

--- src/main/java/org/leetcode/WordLadder.java
+++ src/main/java/org/leetcode/WordLadder.java
@@ -1,69 +1,86 @@
 package org.leetcode;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
-//Wrong answer
-public class WordLadder {
-	
-	private Map<String,Integer> stsMap = new HashMap<String,Integer>();
+/*
+ * Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
 
-	public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
-		int stsLength = 0;
-		if(beginWord.equals(endWord)) return 0;
-		if(getDifference(beginWord, endWord) == 1){
-			return 1;
-		}
-		if(stsMap.containsKey(beginWord)){
-			return stsMap.get(beginWord);
-		}
-		Set<String> related = getDiffersByOneWords(beginWord, wordDict);
-		if(related.size() <= 0) return -1;
-		int minLen = Integer.MAX_VALUE;
-		for(String curWord : related){
-			int curLen = 0;
-			if(getDifference(curWord,endWord) == 1){
-				stsMap.put(curWord, 1);
-				stsLength = 1;
-				break;
-			}else{
-				curLen = 1 + ladderLength(curWord,endWord,wordDict);
-				if(curLen < minLen) stsLength = curLen;
+Only one letter can be changed at a time
+Each intermediate word must exist in the word list
+For example,
+
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note:
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+ */
+class WordStep
+{
+	String word;
+	int numSteps = 0;
+	public WordStep(String str, int steps)
+	{
+		this.word = str;
+		this.numSteps = steps;
+	}
+}
+public class WordLadder
+{
+	//Accepted solution with 85 ms runtime
+	//BFS solution. Credits to programcreek.com
+	public int ladderLength(String beginWord, String endWord, Set<String> wordDict)
+	{
+		List<WordStep> queue = new LinkedList<WordStep>();
+		wordDict.add(endWord);
+		queue.add(new WordStep(beginWord, 1));
+		while(!queue.isEmpty())
+		{
+			WordStep step = queue.remove(0);
+			String curWord = step.word;
+			if(curWord.equals(endWord))
+				return step.numSteps;
+			char[] charArr = curWord.toCharArray();
+			for(int i = 0 ; i < charArr.length; i++)
+			{
+				for(char c = 'a' ; c < 'z' ; c++)
+				{
+					char tmp = charArr[i];
+					charArr[i] = c;
+					String newWord = new String(charArr);
+					if(wordDict.contains(newWord))
+					{
+						queue.add(new WordStep(newWord, step.numSteps+1));
+						wordDict.remove(newWord);
+					}
+					charArr[i] = tmp;
+				}
 			}
 		}
-		
-		return stsLength;
-	}
-	
-	private Set<String> getDiffersByOneWords(String word, Set<String> wordDict){
-		Set<String> strList = new HashSet<String>();
-		for(String curWord : wordDict){
-			if(getDifference(word,curWord) == 1)
-				strList.add(curWord);
-		}
-		return strList;
+		return 0;
 	}
 
-	private int getDifference(String word1, String word2) {
-		int diff = 0;
-		for(int i = 0 ; i < word1.length() ; i++){
-			if(word1.charAt(i) != word2.charAt(i)){
-				diff++;
-			}
-		}
-		return diff;
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		WordLadder obj = new WordLadder();
 		String begin = "hit";
 		String end = "cog";
 		Set<String> wordDict = new HashSet<String>();
 		wordDict.add("hot");wordDict.add("dot");wordDict.add("dog");wordDict.add("lot");wordDict.add("log");
+		long startTime = System.currentTimeMillis();
 		int stsLen = obj.ladderLength(begin, end, wordDict);
-		System.out.println(stsLen);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Ladder length: " + stsLen);
+		System.out.println("Time to find ladder length: " + (endTime - startTime) + " ms");
 	}
 
 }
