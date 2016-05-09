f8549b9f2fc15520a60acfa3164a3172deada258
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-09 11:21:49 -0400

    Modified 'ThreeSum' to an acceptable solution

--- src/main/java/org/leetcode/ThreeSum.java
+++ src/main/java/org/leetcode/ThreeSum.java
@@ -1,6 +1,7 @@
 package org.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -23,9 +24,43 @@ The solution set must not contain duplicate triplets.
  */
 public class ThreeSum
 {
+	//Accepted solution with 9ms runtime
+	//A better solution that is almost O(n^2), but avoids checking duplicates
 	public List<List<Integer>> threeSum(int[] nums)
     {
-		return null;
+		List<List<Integer>> masterList = new ArrayList<List<Integer>>();
+		if(nums == null || nums.length < 3)
+			return masterList;
+		Arrays.sort(nums);
+		for(int i = 0 ; i < nums.length - 1 ; i++)
+		{
+			if(i > 0 && nums[i] == nums[i-1])
+				continue;
+			int lo = i + 1;
+			int hi = nums.length - 1;
+			while(lo < hi)
+			{
+				if(nums[i] + nums[lo] + nums[hi] == 0)
+				{
+					List<Integer> curList = new ArrayList<Integer>();
+					curList.add(nums[i]);
+					curList.add(nums[lo]);
+					curList.add(nums[hi]);
+					masterList.add(curList);
+					lo++;
+					while(nums[lo] == nums[lo - 1] && lo < hi)
+						lo++;
+					hi--;
+					while(nums[hi] == nums[hi+1] && lo < hi)
+						hi--;
+				}
+				else if(nums[i] + nums[lo] + nums[hi] < 0)
+					lo++;
+				else if(nums[i] + nums[lo] + nums[hi] > 0)
+					hi--;
+			}
+		}
+		return masterList;
     }
 	
 	//O(n^2) solution. Time limit exceeded
@@ -69,11 +104,7 @@ public class ThreeSum
 	public static void main(String[] args)
 	{
 		ThreeSum obj = new ThreeSum();
-//		int[] nums = {-1,0,1,2,-1,-4};
-		int[] nums = {13,-5,3,3,-1,13,3,1,-9,-4,9,12,6,-9,-6,-12,-8,3,12,14,4,-15,2,-11,4,-12,10,9,-6,
-				-3,-8,14,7,3,2,-8,-7,-10,8,-8,-7,-6,-11,6,-7,-2,9,-8,8,-2,13,-10,-2,0,-14,-13,-4,11,3,
-				-3,-7,3,-4,8,13,13,-15,-9,10,0,-2,-12,1,2,9,1,8,-4,8,-7,9,7,-2,-15,14,0,-13,-13,-12,-2,
-				-1,-11,8,10,12,6,8,4,12,3,11,-12,-2,-3,5,-15,6,-10,-4,-1,-1,-10,13};
+		int[] nums = {-1,0,1,2,-1,-4};
 		long startTime = System.currentTimeMillis();
 		List<List<Integer>> list = obj.threeSum(nums);
 		long endTime = System.currentTimeMillis();
