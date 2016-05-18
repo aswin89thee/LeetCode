8cf4da52b8988ada85ffdda3a8d0350ad772307a
Aswin Rajamannar <aswin89thee@gmail.com>
2016-05-18 00:25:26 -0400

    Fixed the "Edit Distance" code to an acceptable DP solution

--- src/main/java/org/leetcode/EditDistance.java
+++ src/main/java/org/leetcode/EditDistance.java
@@ -1,84 +1,67 @@
 package org.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+public class EditDistance
+{
 
-
-//Time limit exceeded for "dinitrophenylhydrazine","acetylphenylhydrazine". Plus I doubt if this algo is 100% correct
-public class EditDistance {
-	int[] distances;
-	boolean[] explored;
-	Map<String,Integer> distMap = new HashMap<String,Integer>();
-	
-	public int minDistance(String word1, String word2) {
-        int minDistance = Integer.MAX_VALUE;
-        distances = new int[word2.length()];
-        explored = new boolean[word2.length()];
-        minDistance = calculateMinDist(word1,word2);
-        return minDistance;
+	//Accepted solution with 14 ms runtime
+	//Dp solution based on Wagner-Fischer algorithm : https://en.wikipedia.org/wiki/Edit_distance#Common_algorithm
+    public int minDistance(String word1, String word2)
+    {
+    	if(word1.isEmpty() || word2.isEmpty())
+    		return Integer.max(word1.length(), word2.length());
+    	int[][] distances = new int[word1.length()][word2.length()];
+    	int len1 = word1.length();
+    	int len2 = word2.length();
+    	char[] arr1 = word1.toCharArray();
+    	char[] arr2 = word2.toCharArray();
+    	if(arr1[0] == arr2[0])
+    		distances[0][0] = 0;
+    	else
+    		distances[0][0] = 1;
+    	int i = 0, j = 0;
+    	//Calculate the cost along the edges
+    	//If one of the string has only one character, then all operations are either insert or delete
+    	//These make up our rows and columns
+    	for(i = 1 ; i < len1 ; i++)
+    	{
+    		if(arr1[i] == arr2[0])
+    			distances[i][0] = i;
+    		else
+    			distances[i][0] = Integer.min(i+1, distances[i-1][0] + 1);
+    	}
+    	for(j = 1 ; j < len2 ; j++)
+    	{
+    		if(arr2[j] == arr1[0])
+    			distances[0][j] = j;
+    		else
+    			distances[0][j] = Integer.min(j+1, distances[0][j-1]+1);
+    	}
+    	
+    	//Fill the inner distance matrix
+    	for(i = 1 ; i < len1 ; i++)
+    	{
+    		for(j = 1; j < len2 ; j++)
+    		{
+    			if(arr1[i] == arr2[j])
+    			{
+    				distances[i][j] = distances[i-1][j-1];
+    				continue;
+    			}
+    			int d1 = distances[i-1][j-1];
+    			int d2 = distances[i-1][j];
+    			int d3 = distances[i][j-1];
+    			int min = Integer.min(d1, d2);
+    			min = Integer.min(min, d3);
+    			distances[i][j] = min + 1;
+    		}
+    	}
+    	return distances[len1-1][len2-1];
     }
-
-	private int calculateMinDist(String word1, String word2) {
-		if(word1.equals(word2)) return 0;
-		if(word1 == null || word1.equals("")) return word2.length();
-		if(word2 == null || word2.equals("")) return word1.length();
-		if(distMap.get(word1) != null) return distMap.get(word1);
-		int i;
-		int minDist = Integer.MAX_VALUE;
-		for(i = 0 ; i < word1.length() ; i++){
-			if(i == word2.length()){
-				minDist = word1.length() - word2.length();
-				return minDist;
-			}
-			if(word1.charAt(i) == word2.charAt(i)){
-				continue;
-			}else{
-				if(explored[i] && word1.length() >= word2.length()) return distances[i];
-				//Characters are different. Try different combinations
-				char ch1 = word1.charAt(i), ch2 = word2.charAt(i);
-				//Replace
-				String newWord = word1.substring(0,i) + ch2 + word1.substring(i+1);
-				int distReplace = 1 + calculateMinDist(newWord, word2);
-				distMap.put(newWord, distReplace);
-				//Delete
-				newWord = word1.substring(0,i)+word1.substring(i+1);
-				int distDelete = 1 + calculateMinDist(newWord, word2);
-				distMap.put(newWord, distDelete);
-				//Insert before and after
-				if(word1.length() < word2.length()){
-					newWord = word1.substring(0,i) + ch2 + "" + word1.substring(i);
-					int distInsert1 = 1 + calculateMinDist(newWord,word2);
-					distMap.put(newWord, distInsert1);
-					newWord = word1.substring(0,i) + ch1 + "" + ch2 + word1.substring(i+1);
-					int distInsert2 = 1 + calculateMinDist(newWord ,word2);
-					distMap.put(newWord, distInsert2);
-					minDist = getMinVal(distReplace, distDelete, distInsert1,distInsert2);
-				}else{
-					minDist = distReplace < distDelete ? distReplace : distDelete;
-				}
-				explored[i] = true;
-				distances[i] = minDist;
-				distMap.put(word1, minDist);
-				return minDist;
-			}
-		}
-		minDist = word2.length() - word1.length();
-		explored[i] = true;
-		distances[i] = minDist;
-		return minDist;
-	}
-
-	private int getMinVal(int a, int b, int c, int d) {
-		int min = a;
-		min = min < b ? min : b;
-		min = min < c ? min : c;
-		min = min <d ? min : d;
-		return min;
-	}
-
-	public static void main(String[] args) {
+    
+	public static void main(String[] args)
+	{
 		EditDistance obj = new EditDistance();
-		System.out.println(obj.minDistance("dinitrophenylhydrazine", "acetylphenylhydrazine"));
+		System.out.println(obj.minDistance("kitten", "sitting"));
 	}
 
 }
