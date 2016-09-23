bf53e42483136ae52540eff4a1747dcfaafffbfb
Aswin Rajamannar <aswin89thee@gmail.com>
2016-09-23 10:53:22 -0400

    Added another solution to "Valid Anagram"

--- src/main/java/org/leetcode/ValidAnagram.java
+++ src/main/java/org/leetcode/ValidAnagram.java
@@ -1,13 +1,26 @@
 package org.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ValidAnagram
 {
+	
+	// Accepted solution with 7 ms runtime. O(1) space
+	public boolean isAnagram(String s, String t)
+    {
+		if(s == null && t == null) return true;
+		if(s == null || t == null) return false;
+		char[] sarr = s.toCharArray();
+		char[] tarr = t.toCharArray();
+		Arrays.sort(sarr);
+		Arrays.sort(tarr);
+		return new String(sarr).equals(new String(tarr));
+    }
 
-	//Accepted solution with 50 ms runtime
-    public boolean isAnagram(String s, String t)
+	//Accepted solution with 50 ms runtime. O(n) space
+    public boolean isAnagram2(String s, String t)
     {
     	if(s.length() != t.length())
     		return false;
